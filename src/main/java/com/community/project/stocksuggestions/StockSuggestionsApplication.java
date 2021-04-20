ffbe681f04e51bdd746b31d88994f6ab58cc2f28
Dipinit <bobaseoul@hotmail.fr>
2021-04-20 23:14:01 +0200

    Add base files from Spring Initializr

---
+++ src/main/java/com/community/project/stocksuggestions/StockSuggestionsApplication.java
@@ -0,0 +1,13 @@
+package com.community.project.stocksuggestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSuggestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSuggestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/community/project/stocksuggestions/StockSuggestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.project.stocksuggestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSuggestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
